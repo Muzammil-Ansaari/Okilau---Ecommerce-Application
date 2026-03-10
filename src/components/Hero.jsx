452d687625f93e45de5e88024f02055d0a969928
Muzammil Ansaari <muzammilansaari6@gmail.com>
2026-03-10 11:08:31 +0500

    fix(hero): fix mobile responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,33 +5,33 @@ import Button from "./UI/Button";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-150 overflow-hidden">
+    <section className="sm:relative w-full h-150 overflow-hidden">
 
   {/* Hero Image */}
   <img
     src={assets.heroImg}
     alt="Okilau Hero"
-    className="absolute inset-0 w-full h-full object-cover object-right sm:object-center"
+    className="sm:absolute sm:inset-0 w-full h-84 sm:h-screen object-cover object-[70%_center] sm:object-center"
   />
 
   {/* Text Content — Left Side */}
-  <div className="absolute inset-0 flex items-center">
-    <div className="px-10 sm:px-16 lg:px-24">
+  <div className="mt-5 sm:mt-0 sm:absolute sm:inset-0 flex items-center justify-center sm:justify-start">
+    <div className="px-4 md:px-8 lg:px-24">
 
       {/* Heading */}
-      <h1 className="font-['Anton'] text-3xl sm:text-4xl lg:text-6xl font-semibold tracking-wider lg:leading-18 text-black uppercase mb-4">
+      <h1 className="font-['Anton'] text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold tracking-widest sm:leading-11 md:leading-13 lg:leading-16 text-black uppercase mb-4 text-center sm:text-left ">
         Oversized <br />
         Printed T-Shirts
       </h1>
 
       {/* Paragraph */}
-      <p className="text-sm sm:text-base font-medium text-black mb-8">
+      <p className="text-base sm:text-base font-medium text-black mb-8 text-center sm:text-left">
         From millions of things designed and sold by artists.
       </p>
 
       {/* Shop Now Button */}
       <Link to="/products">
-        <Button variant="black">Shop Now</Button>
+        <Button className="w-full sm:w-fit" variant="black">Shop Now</Button>
       </Link>
 
     </div>
