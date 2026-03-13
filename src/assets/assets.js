55e1666bc6b3ebe8bf1e374240e680ece9e1b2c6
Muzammil Ansaari <muzammilansaari6@gmail.com>
2026-03-13 15:40:47 +0500

    updated assets

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,12 +2,6 @@ import logo from "./images/logo.png";
 import heroImg from "./images/hero-img.jpeg";
 import banner1 from "./images/banner1.jpeg";
 import banner2 from "./images/banner2.jpeg";
-import mug from "./images/mug.jpeg";
-import marks from "./images/marks.jpeg";
-import doormat from "./images/doormat.jpeg";
-import blanket from "./images/blanket.jpeg";
-import apparel from "./images/apparel.jpeg";
-import phone_cases from "./images/phone-cases.jpeg";
 import promo_card_01 from "./images/promo_card_01.jpeg";
 import promo_card_02 from "./images/promo_card_02.jpeg";
 import promo_card_03 from "./images/promo_card_03.jpeg";
@@ -53,12 +47,6 @@ export const assets = {
   heroImg,
   banner1,
   banner2,
-  mug,
-  marks,
-  doormat,
-  apparel,
-  blanket,
-  phone_cases,
   promo_card_01,
   promo_card_02,
   promo_card_03,
